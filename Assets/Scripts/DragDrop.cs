90194e3ff9d3bbbaa7548111bc4258824ea87d07
Zara <zaraamernaveed@hotmail.co.uk>
2022-02-28 20:27:46 +0000

    BDG-47
    added a 'drop-sone' in the middle that cards from both hands can be dragged and dropped into. currently no ability to bring the cards back to hand - not sure if needed. in project settings a card layer was created for better handling of collisions.

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -7,6 +7,9 @@ public class DragDrop : MonoBehaviour
     //
     private bool _isDragging = false;
     private Vector2 _startPosition;
+    private bool _isOverDropZone = false;
+    private GameObject _dropZone;
+   
     //
     
     //
@@ -20,9 +23,23 @@ public class DragDrop : MonoBehaviour
     //
 
     //
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isOverDropZone = true;
+        _dropZone = collision.gameObject;
+    }
+    //
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _isOverDropZone = false;
+        _dropZone = null;
+    }
+
+    //
     public void StartDrag()
     {
-        _startPosition = transform.position;
+        _startPosition = transform.position; // logs the start position (where the card would spawn)
         _isDragging = true;
     }
     //
@@ -31,6 +48,16 @@ public class DragDrop : MonoBehaviour
     public void EndDrag()
     {
         _isDragging = false;
+
+        if (_isOverDropZone)
+        {
+            transform.SetParent(_dropZone.transform, false);
+
+        }
+        else
+        {
+            transform.position = _startPosition; // if the card isnt over the dropzone snap it back to the start pos
+        }
     }
     //
 }

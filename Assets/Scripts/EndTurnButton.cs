49eba834cbcc4dfa67414afcaa425ad33eac67b0
Joe Mackle <20073322+joe-mac-design@users.noreply.github.com>
2022-03-23 13:48:26 +0000

    BDG-112
    [Added] Scripting for Main Menu
    [Fixed] Draw Cards system
    [Adjusted] Created sub-folder for Menu Scripts

--- Assets/Scripts/EndTurnButton.cs
+++ Assets/Scripts/EndTurnButton.cs
@@ -17,23 +17,23 @@ public class EndTurnButton : MonoBehaviour
         if (_gameManager.PlayersTurn) // if it is the players turn that is ending
         {
             _gameManager.ManaIncrease(_gameManager.Player);
-            /*
+            
             if (_gameManager.PlayerStatsInstance.Cards.Count < 7) // if the player has space, draw 1 new card
             {
                 _gameManager.DrawCard(_gameManager.Player);
             }
-            */
+            
         } 
 
         if (!_gameManager.PlayersTurn)// if it is the opponents turn that is ending
         {
             _gameManager.ManaIncrease(_gameManager.Opponent);
-            /*
+            
             if (_gameManager.OppStatsInstance.Cards.Count < 7)
             {
                 _gameManager.DrawCard(_gameManager.Opponent);
             }
-            */
+            
         }
 
         _gameManager.ClearCardSelection(); // clear the card list

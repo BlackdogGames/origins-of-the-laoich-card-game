7bc3b4716b9e41d0f0dd035f73627fe3d27459f4
Joe Mackle <20073322+joe-mac-design@users.noreply.github.com>
2022-04-12 23:00:35 +0100

    BDG-120
    [Added] Loading Scene random text
    [Fixed] Game Scene settings menu leave button

--- Assets/Scripts/MenuScripts/SceneLoader.cs
+++ Assets/Scripts/MenuScripts/SceneLoader.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneLoader : MonoBehaviour
 {
     public GameObject LoadingScreen;
     public Slider LoadingBar;
+
+    public string[] LoadingTips = { "Tip 01", "Tip 02", "Tip 03", "Tip 04", "Tip 05", "Tip 06", "Tip 07", "Tip 08", "Tip 09", "Tip 10" };
+    public TMP_Text LoadingText;
+
+    private void Start()
+    {
+        string TextToDisplay = RandomTip();
+
+        LoadingText.text = TextToDisplay;
+    }
+
+    private string RandomTip()
+    {
+        string RandomTip = LoadingTips[Random.Range(0, LoadingTips.Length)];
+        return RandomTip;
+    }
 
     public void LoadScene(string LevelName)
     {
